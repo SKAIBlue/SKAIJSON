2a29fe4ffae519bd3e3b26e37b47381242b012de
Jong-Woo Park <jwmtp2@gmail.com>
2018-05-16 23:16:32 +0900

    init

---
+++ skaijsonlib/src/main/java/com/skaiblue/skaijson/SKAIJSON.java
@@ -0,0 +1,26 @@
+package com.skaiblue.skaijson;
+
+import java.util.List;
+
+public class SKAIJSON {
+
+
+    public Object toObject(String str, Class T)
+    {
+        return null;
+    }
+
+
+    public List toArray(String str, Class T)
+    {
+        return null
+    }
+
+
+    public interface OnJSONParseCompleted
+    {
+
+    }
+
+
+}
